ca17ba33f141509699bb2c8e14c56e6b1f84d62b
Suvam Basak <suvambasak2008@gmail.com>
2021-11-23 19:27:56 +0530

    init

---
+++ dht11.py
@@ -0,0 +1,27 @@
+import Adafruit_DHT
+from urllib.request import urlopen
+import json
+from time import sleep
+
+
+conf = json.loads(open('conf.json').read())
+sensor = Adafruit_DHT.DHT11
+gpio = conf['pin']
+ip = conf['ip']
+
+
+while True:
+    try:
+        sleep(2)
+        humidity, temperature = Adafruit_DHT.read_retry(sensor, gpio)
+        # print(humidity, temperature)
+
+        # http://192.168.0.103/sensor/1.0
+        URL = 'http://'+ip+':80/sensor/'+str(temperature)
+
+        with urlopen(URL) as url:
+            data = json.loads(url.read().decode())
+            # print(data)
+    except Exception as e:
+        # print(str(e))
+        pass
